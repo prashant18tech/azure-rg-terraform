ee0d2cf90fa3593d26d4bf6bccd4e08451edfee5
prashant18tech <65825412+prashant18tech@users.noreply.github.com>
2025-11-02 13:44:53 +0530

    03-Add new variables for storage account configuration

--- variables.tf
+++ variables.tf
@@ -8,3 +8,26 @@ variable "resource_group_name" {
 variable "location" {
   default = "East US"
 }
+
+variable "storage_account_name" {
+  description = "Name of the Storage Account"
+  type        = string
+}
+
+variable "account_tier" {
+  description = "Storage Account tier"
+  type        = string
+  default     = "Standard"
+}
+
+variable "account_replication_type" {
+  description = "Storage Account replication type"
+  type        = string
+  default     = "LRS"
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
+}
